1f3d17d78d3fb1321d7d810d87057eff6cbbe095
lisajung <lisajung998@gmail.com>
2023-04-26 02:11:00 -0700

    middleware example

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
+
+
+
+// only for authenticated users
+app.use((req, res, next) => {
+    console.log('middleware');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('<h1> Hello World <h1>');
 });
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
+app.get('/anotherRoute', (req, res, next) => {
+    console.log(1);
+    next()
+}, (req, res) => {
+    console.log(2);
+    res.send('<h1> anotherRoute <h1>');
 });
+
+
+
 
