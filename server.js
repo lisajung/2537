bcd8d10c26393f38ca97f8bfec4e1c41d372e5fa
lisajung <lisajung998@gmail.com>
2023-04-28 09:42:24 -0700

    connected mongodb database

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = require('./app');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
+    await mongoose.connect('mongodb+srv://lisajung998:7IlSIcnoUP5ooGwa@cluster0.pkjtbxx.mongodb.net/comp2537w1?retryWrites=true&w=majority');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
     console.log("connected to db");
